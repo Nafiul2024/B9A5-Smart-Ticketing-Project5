b08217fea25156a458cb380ff8c5cbcb3565b7ac
Nafiul2024 <nafiulislam620@gmail.com>
2024-02-19 19:59:25 +0600

    Assignment Done According To My Ability

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 let count =0 ;
 const allBtn = document.getElementsByClassName('btn');
     for(const btn of allBtn){
-        btn.addEventListener("click", function handleSelect(event){
+        btn.addEventListener("click", function handleSelect(){
             const price = document.getElementById('price');
             const convertedPrice = parseInt(price.innerText);
             const priceContainer = document.getElementById("price-container");
             const li = document.createElement("li");
             const p = document.createElement("p");
-            p.innerText = convertedPrice;
+            const p1 = document.createElement("p");
+            const p2 = document.createElement("p");
+
+            p1.innerText = 'Seat Number';
+            p2.innerText = 'Economy Class';
+            p.innerText = convertedPrice + "Taka Only";
+            li.appendChild(p1);
+            li.appendChild(p2);
             li.appendChild(p);
             priceContainer.appendChild(li);
 
@@ -16,15 +23,29 @@ const allBtn = document.getElementsByClassName('btn');
             const total = parseFloat(totalPrice) + parseFloat(price.innerText);
             document.getElementById("total-price").innerText = total;
 
-            // count = count + 1;
+            // Grand-Total-Price
+            const grandTotalPrice = document.getElementById("grand-total").innerText;
+            const grandTotal = parseFloat(grandTotalPrice) + parseFloat(price.innerText);
+            document.getElementById("grand-total").innerText = grandTotal;
+            
 
             const totalTicket = document.getElementById("total-tickets").innerText;
             const totalT = parseInt(totalTicket) + parseInt(count+1);
             document.getElementById("total-tickets").innerText = totalT;
 
+
             const seatsTotal = document.getElementById("seats-left").innerText;
             const seatsLeft = parseInt(seatsTotal) + parseInt(count-1);
-            document.getElementById("seats-left").innerText = seatsLeft;
+            document.getElementById("seats-left").innerText = seatsLeft ;
+
+
+            const couponBtn = document.getElementById('coupon-input');
+            const couponButton = couponBtn.value;
+            if(couponButton === 'NEW15' || couponButton === 'Couple20'){
+              couponBtn.classList.add('hidden');
+            }else{
+              return ('Invalid Coupon');
+            }
 
             
 
@@ -33,16 +54,54 @@ const allBtn = document.getElementsByClassName('btn');
         })
     }
 
-    function grandTotal(category) {
-        const convertTotal = getValue(total);
-        if (category === "NEW15") {
-          setInnerText("grand-total", convertTotal *0.85);
-        } else if (category === "Couple20") {
-          setInnerText("grand-total", convertTotal *0.85);
+
+    
+        function grandTotal(couponInput) {
+        const grandTotal = document.getElementById('grand-total');
+        const convertedGrandTotal = parseFloat(grandTotal.innerText);
+        const totalPrice = document.getElementById("total-price").innerText;
+        const total = parseFloat(totalPrice) + parseFloat(price.innerText);
+        document.getElementById("total-price").innerText = total;
+
+        if (couponInput === "NEW15") {
+          setInnerText("grand-total", total *0.85);
+        } else if (couponInput === "Couple20") {
+          setInnerText("grand-total", total *0.80);
         } else {
-          setInnerText("grand-total", convertTotal);
+          setInnerText("grand-total", convertedGrandTotal);
         }
       }
+
+
       function setInnerText(id, value) {
         value = document.getElementById(id).innerText;
       }
+
+
+      const couponBtn = document.getElementById('coupon-button');
+      couponBtn.addEventListener('click', function() {
+        let couponInput = document.getElementById('coupon-input').value;
+        if(couponInput === 'NEW15' || couponInput === 'Couple20'){
+          couponBtn.classList.add('hidden');
+          couponInput.classList.add('hidden');
+        }else{
+          alert ("Invalid Coupon");
+        }
+        return couponInput;
+ 
+      } )  
+
+
+
+      function disableBtn(){
+        document.getElementById('btn').disabled = true;
+    }
+
+
+    const allBttn = document.getElementsByClassName('btn');
+    for(const btn of allBttn){
+    btn.addEventListener("click", function buttonBackgroundColor(){
+    btn.classList.add('bg-[#1DD100]');
+    
+  })
+}
